907b935835985aaedfbf1dbef50e21a9e774af4e
NickTheo96 <nicktheodoulou96@gmail.com>
2018-10-04 18:52:10 +0100

    - Currently putting files into dataframes

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import pandas as pd
+import glob
+import os
+import numpy as np
+
+
 def main():
-    print('Hello World')
+    list_of_files = glob.glob('Data_In/Train/neg/*.txt')
+
+    a = pd.read_csv(list_of_files[0], dtype={'ID': object})
+    b = pd.read_csv(list_of_files[1], dtype={'ID': object})
+
+    a.to_csv('Data_Out/a.csv', index=1)
+    b.to_csv('Data_Out/b.csv', index=2)
+
+    print(a.shape)
+
+    d = pd.concat([a, b], sort=False)
+    d.to_csv('Data_Out/d.csv')
 
 
 if __name__ == "__main__":
